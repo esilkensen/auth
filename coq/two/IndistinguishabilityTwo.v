21ebe0caa8f32956f3b520801102d2927999d13c
Erik Silkensen <silkense@colorado.edu>
2013-10-24 02:56:46 -0400

    the blues are still blue

--- coq/two/IndistinguishabilityTwo.v
+++ coq/two/IndistinguishabilityTwo.v
@@ -11,7 +11,7 @@ Context (L : two)
         (P : value -> value -> Prop)
         (Prefl : forall x, P x x).
 
-Fixpoint atom_Lequiv a1 a2 : Prop :=
+Fixpoint atom_LPequiv a1 a2 : Prop :=
   match a1, a2 with
     | Atom v1 l1, Atom v2 l2 =>
       (L = Bottom2 /\ l1 = l2 /\ l1 = Top2 /\
@@ -19,32 +19,32 @@ Fixpoint atom_Lequiv a1 a2 : Prop :=
          | VNat n1, VNat n2 => P v1 v2
          | _, _ => True
        end) \/
-      (L = Bottom2 /\ l1 = l2 /\ l1 = Bottom2 /\ value_Lequiv v1 v2) \/
-      (L = Top2 /\ l1 = l2 /\ value_Lequiv v1 v2)
+      (L = Bottom2 /\ l1 = l2 /\ l1 = Bottom2 /\ value_LPequiv v1 v2) \/
+      (L = Top2 /\ l1 = l2 /\ value_LPequiv v1 v2)
   end
-with value_Lequiv v1 v2 : Prop :=
+with value_LPequiv v1 v2 : Prop :=
        match v1, v2 with
          | VNat n1, VNat n2 => n1 = n2
          | VClos e1 t1, VClos e2 t2 =>
-           list_forall2 atom_Lequiv e1 e2 /\ term_equiv t1 t2
+           list_forall2 atom_LPequiv e1 e2 /\ term_equiv t1 t2
          | VNat _, _ | VClos _ _, _ => False
        end.
 
-Definition env_Lequiv : env -> env -> Prop :=
-  list_forall2 atom_Lequiv.
+Definition env_LPequiv : env -> env -> Prop :=
+  list_forall2 atom_LPequiv.
 
-Lemma atom_Lequiv_lab_inv :
+Lemma atom_LPequiv_lab_inv :
   forall v1 v2 l1 l2,
-    atom_Lequiv (Atom v1 l1) (Atom v2 l2) ->
+    atom_LPequiv (Atom v1 l1) (Atom v2 l2) ->
     l1 = l2.
 Proof.
   intros. inversion H; inversion H0; destruct_conjs; auto.
 Qed.
 
-Lemma atom_Lequiv_raise :
+Lemma atom_LPequiv_raise :
   forall v1 v2,
-    atom_Lequiv (Atom v1 Bottom2) (Atom v2 Bottom2) ->
-    atom_Lequiv (Atom v1 Top2) (Atom v2 Top2).
+    atom_LPequiv (Atom v1 Bottom2) (Atom v2 Bottom2) ->
+    atom_LPequiv (Atom v1 Top2) (Atom v2 Top2).
 Proof.
   intros v1 v2 H. destruct L.
     destruct v1; destruct v2; destruct H; destruct H; destruct_conjs;
@@ -62,3 +62,43 @@ Proof.
 Qed.
 
 End IndistinguishabilityTwo.
+
+Definition atom_Lequiv := fun L => atom_LPequiv L (fun _ _ => True).
+
+Definition value_Lequiv := fun L => value_LPequiv L (fun _ _ => True).
+
+Definition env_Lequiv := fun L => env_LPequiv L (fun _ _ => True).
+
+Lemma atom_value_env_LPequiv_Lequiv :
+  forall L P,
+    (forall a1 a2,
+       atom_LPequiv L P a1 a2 -> atom_Lequiv L a1 a2)
+    /\ (forall v1 v2,
+          value_LPequiv L P v1 v2 -> value_Lequiv L v1 v2)
+    /\ (forall e1 e2,
+          env_LPequiv L P e1 e2 -> env_Lequiv L e1 e2).
+Proof.
+  Admitted. (* TODO *)
+
+Lemma atom_LPequiv_Lequiv :
+  forall L P a1 a2,
+    atom_LPequiv L P a1 a2 -> atom_Lequiv L a1 a2.
+Proof. apply atom_value_env_LPequiv_Lequiv. Qed.
+
+Lemma value_LPequiv_Lequiv :
+  forall L P v1 v2,
+    value_LPequiv L P v1 v2 -> value_Lequiv L v1 v2.
+Proof. apply atom_value_env_LPequiv_Lequiv. Qed.
+
+Lemma env_LPequiv_Lequiv :
+  forall L P e1 e2,
+    env_LPequiv L P e1 e2 -> env_Lequiv L e1 e2.
+Proof. apply atom_value_env_LPequiv_Lequiv. Qed.
+
+Lemma atom_Lequiv_lab_inv :
+  forall L v1 v2 l1 l2,
+    atom_Lequiv L (Atom v1 l1) (Atom v2 l2) ->
+    l1 = l2.
+Proof.
+  intros. inversion H; inversion H0; destruct_conjs; auto.
+Qed.
--- coq/two/LambdaTwoNI.v
+++ coq/two/LambdaTwoNI.v
@@ -34,11 +34,11 @@ Qed.
 Lemma NI_strong :
   forall pc1 pc2 e1 e2 t1 t2 a1 a2,
     pc1 = pc2 ->
-    env_Lequiv L P e1 e2 ->
+    env_LPequiv L P e1 e2 ->
     term_equiv t1 t2 ->
     pc1; e1 ⊢ t1 ⇓ a1 ->
     pc2; e2 ⊢ t2 ⇓ a2 ->
-    atom_Lequiv L P a1 a2.
+    atom_Lequiv L a1 a2.
 Proof.
   intros pc1 pc2 e1 e2 t1 t2 a1 a2 Hpc He Ht Heval.
   generalize dependent a2.
@@ -47,32 +47,35 @@ Proof.
   generalize dependent pc2.
   induction Heval;
     intros pc2 Hpc e2 He t2' Ht a3' Heval2'; destruct t2'; try inversion Ht.
-    (* Eval_nat *)
+  - (* Eval_nat *)
     inversion Heval2'. subst.
     destruct L; destruct pc2.
       right. left. auto.
       left. auto.
       right. right. auto.
       right. right. auto.
-    (* Eval_var *)
-    inversion Heval2'. subst. unfold env_Lequiv in He.
-    assert (atom_Lequiv L P (Atom v l) (Atom v0 l0)) by
+  - (* Eval_var *)
+    inversion Heval2'. subst. unfold env_LPequiv in He.
+    assert (atom_LPequiv L P (Atom v l) (Atom v0 l0)) by
         (eapply list_forall2_atIndex; eassumption).
     assert (l = l0) by
-        (eapply atom_Lequiv_lab_inv; eassumption).
+        (eapply atom_LPequiv_lab_inv; eassumption).
     subst.
     destruct pc2.
-      rewrite join_bot. assumption.
+      rewrite join_bot. eapply atom_LPequiv_Lequiv. eassumption.
       rewrite join_top. destruct l0.
-        apply atom_Lequiv_raise. apply Prefl. assumption. assumption.
-    (* Eval_lam *)
+        apply atom_LPequiv_raise in H0. eapply atom_LPequiv_Lequiv. eassumption.
+          assumption.
+        eapply atom_LPequiv_Lequiv. eassumption.
+  - (* Eval_lam *)
     inversion Heval2'. subst.
+    apply env_LPequiv_Lequiv in He.
     destruct L; destruct pc2.
-      right. left. auto.
+      right. left. auto. 
       left. auto.
       right. right. auto.
       right. right. auto.
-    (* Eval_app *)
+  - (* Eval_app *)
     inversion Heval2'. subst.
     rename l0 into l1'. rename a0 into a2'.
     assert (t2'1 = t1) by
@@ -80,45 +83,18 @@ Proof.
     assert (t2'2 = t2) by
         (apply term_equiv_eq; symmetry; auto).
     subst. clear H. clear H0. clear Ht.
-    remember (Atom (VClos e1' t1') l1) as f1.
-    remember (Atom (VClos e1'0 t1'0) l1') as f2.
-    assert (atom_Lequiv L P f1 f2) by
-        (eapply IHHeval1; eauto). subst.
-    assert (l1' = l1) by
-        (apply atom_Lequiv_lab_inv in H; auto). subst.
-    destruct L.
-      destruct l1.
-        destruct H; destruct H; destruct_conjs; try inversion H1; inversion H.
-          assert (atom_Lequiv Bottom2 P a2 a2').
-            apply IHHeval2 in H6; auto.
-          assert (env_Lequiv Bottom2 P e1' e1'0) by auto.
-          assert (env_Lequiv Bottom2 P (a2 :: e1') (a2' :: e1'0)).
-            split; auto. 
-          apply IHHeval3 in H8; auto.
-        destruct a3 as [v3 l3]; destruct v3; destruct l3;
-        destruct a3' as [v3' l3']; destruct v3'; destruct l3';
-          apply eval_pc_lower_bound in H8; inversion H8;
-          apply eval_pc_lower_bound in Heval3; inversion Heval3;
-          left; auto; repeat (split; auto).
-        admit. (* TODO: L=Bottom2 l1=Top2, need P (VNat n) (VNat n0) *)
-      assert (env_Lequiv Top2 P e1' e1'0) by
-            (destruct H; destruct H; destruct_conjs; inversion H; auto).
-      assert (atom_Lequiv Top2 P a2 a2') by
-            (apply IHHeval2 in H6; auto).
-      assert (env_Lequiv Top2 P (a2 :: e1') (a2' :: e1'0)) by
-          (split; auto).
-      assert (term_equiv t1' t1'0) by
-          (destruct H; destruct H; destruct_conjs; inversion H; auto).
-      apply IHHeval3 in H8; auto.
-Qed.
+    assert (l1' = l1).
+      apply IHHeval1 in H3. apply atom_Lequiv_lab_inv in H3.
+        symmetry. assumption. reflexivity. assumption. reflexivity. subst.
+    Admitted.
       
 (** * General non-interference theorem. *)
 Theorem general_non_interference :
   forall pc e1 e2 t a1 a2,
-    env_Lequiv L P e1 e2 ->
+    env_LPequiv L P e1 e2 ->
     pc; e1 ⊢ t ⇓ a1 ->
     pc; e2 ⊢ t ⇓ a2 ->
-    atom_Lequiv L P a1 a2.
+    atom_Lequiv L a1 a2.
 Proof. intros. eapply NI_strong; eauto. Qed.
 
 End NI.
