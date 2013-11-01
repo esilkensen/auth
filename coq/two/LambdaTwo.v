dbdfbbc4760aa916185ed3f9a7f5661fed443ba3
Erik Silkensen <silkense@colorado.edu>
2013-11-01 05:19:39 -0400

    new idea

--- coq/two/LambdaTwo.v
+++ coq/two/LambdaTwo.v
@@ -1,48 +1,79 @@
 Require Export LambdaTwoSyntax.
 Require Export IndistinguishabilityTwo.
 
-Reserved Notation "[ L , P , k ] pc ; e ⊢ t ⇓ a" (at level 70).
+Reserved Notation "[ L , P ] pc ; e ⊢ t ⇓ a" (at level 70).
 Inductive eval :
-  two -> (value -> value -> Prop) -> nat ->
-  two -> env -> term -> atom -> Prop :=
+  two -> (value -> value -> Prop) ->
+  two -> env -> term -> option atom -> Prop :=
 
 | E_Nat : forall L P pc e n,
 (* ------------------------------------------------------ *)
-    [L, P, 0] pc; e ⊢ TNat n ⇓ Atom (VNat n) pc
+    [L, P] pc; e ⊢ TNat n ⇓ Some (Atom (VNat n) pc)
 
 | E_Var : forall L P pc e n v l,
     atIndex e n = Some (Atom v l) ->
 (* ------------------------------------------------------ *)
-    [L, P, 0] pc; e ⊢ TVar n ⇓ Atom v (l ⊔ pc)
+    [L, P] pc; e ⊢ TVar n ⇓ Some (Atom v (l ⊔ pc))
 
 | E_Abs : forall L P pc e t,
 (* ------------------------------------------------------ *)
-    [L, P, 0] pc; e ⊢ TLam t ⇓ Atom (VClos e t) pc
+    [L, P] pc; e ⊢ TLam t ⇓ Some (Atom (VClos e t) pc)
 
-| E_App : forall L P k pc e t1 t2 e1' t1' l1 a2 a3,
-    [L, P, k] pc; e ⊢ t1 ⇓ Atom (VClos e1' t1') l1 ->
-    [L, P, k] pc; e ⊢ t2 ⇓ a2 ->
-    [L, P, k] l1; a2 :: e1' ⊢ t1' ⇓ a3 ->
+| E_App : forall L P pc e t1 t2 e1' t1' l1 a2 a3,
+    [L, P] pc; e ⊢ t1 ⇓ Some (Atom (VClos e1' t1') l1) ->
+    [L, P] pc; e ⊢ t2 ⇓ Some a2 ->
+    [L, P] l1; a2 :: e1' ⊢ t1' ⇓ Some a3 ->
 (* ------------------------------------------------------ *)
-    [L, P, S k] pc; e ⊢ TApp t1 t2 ⇓ a3
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ Some a3
 
-| E_Decl1 : forall L P k pc e t1 t2 v,
-    [L, P, k] pc; e ⊢ TApp t1 t2 ⇓ Atom v Bottom2 ->
+(*| E_App1 : forall L P pc e t1 t2,
+    [L, P] pc; e ⊢ t1 ⇓ None ->
 (* ------------------------------------------------------ *)
-    [L, P, S k] pc; e ⊢ TDecl t1 t2 ⇓ Atom v Bottom2
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ None*)
 
-(*| E_Decl2 : forall L P k pc e t1 t2 e1' t1' l1 a2 v3,
-    [L, P, k] pc; e ⊢ t1 ⇓ Atom (VClos e1' t1') l1 ->
-    [L, P, k] pc; e ⊢ t2 ⇓ a2 ->
-    [L, P, k] l1; a2 :: e1' ⊢ t1' ⇓ Atom v3 Top2 ->
-    (forall a a' v v',
-       atom_LPequiv L P a a' ->
-       [L, P, k] l1; a :: e1' ⊢ t1' ⇓ Atom v Top2 ->
-       [L, P, k] l1; a' :: e1' ⊢ t1' ⇓ Atom v' Top2 ->
-       value_Lequiv L v v') ->
+(*| E_App2 : forall L P pc e t1 t2, 
+    [L, P] pc; e ⊢ t2 ⇓ None ->
 (* ------------------------------------------------------ *)
-    [L, P, S k] pc; e ⊢ TDecl t1 t2 ⇓ Atom v3 Bottom2*)
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ None*)
 
-where "[ L , P , k ] pc ; e ⊢ t ⇓ a" := (eval L P k pc e t a).
+(*| E_App3 : forall L P pc e t1 t2 e1' t1' l1 a2,
+    [L, P] pc; e ⊢ t1 ⇓ Some (Atom (VClos e1' t1') l1) ->
+    [L, P] pc; e ⊢ t2 ⇓ Some a2 ->
+    [L, P] l1; a2 :: e1' ⊢ t1' ⇓ None ->
+(* ------------------------------------------------------ *)
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ None*)
+
+| E_Decl1 : forall L P pc e t1 t2 v,
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ Some (Atom v Bottom2) ->
+(* ------------------------------------------------------ *)
+    [L, P] pc; e ⊢ TDecl t1 t2 ⇓ Some (Atom v Bottom2)
+
+(*| E_Decl : forall L P pc e t1 t2,
+    [L, P] pc; e ⊢ TApp t1 t2 ⇓ None ->
+(* ------------------------------------------------------ *)
+    [L, P] pc; e ⊢ TDecl t1 t2 ⇓ None*)
+
+| E_Decl2 : forall L P pc e t1 t2 e1' t1' l1 a2 v3,
+    [L, P] pc; e ⊢ t1 ⇓ Some (Atom (VClos e1' t1') l1) ->
+    [L, P] pc; e ⊢ t2 ⇓ Some a2 ->
+    [L, P] l1; a2 :: e1' ⊢ t1' ⇓ Some (Atom v3 Top2) ->
+    forall a2' e2' v3',
+      env_LPequiv L P (a2 :: e1') (a2' :: e2') ->
+      [L, P] l1; a2' :: e2' ⊢ t1' ⇓ Some (Atom v3' Top2) ->
+      value_LPequiv L P v3 v3' ->
+(* ------------------------------------------------------ *)
+    [L, P] pc; e ⊢ TDecl t1 t2 ⇓ Some (Atom v3 Bottom2)
+
+| E_Decl21 : forall L P pc e t1 t2 e1' t1' l1 a2 v3,
+    [L, P] pc; e ⊢ t1 ⇓ Some (Atom (VClos e1' t1') l1) ->
+    [L, P] pc; e ⊢ t2 ⇓ Some a2 ->
+    [L, P] l1; a2 :: e1' ⊢ t1' ⇓ Some (Atom v3 Top2) ->
+    forall a2' e2',
+      env_LPequiv L P (a2 :: e1') (a2' :: e2') ->
+      [L, P] l1; a2' :: e2' ⊢ t1' ⇓ None ->
+(* ------------------------------------------------------ *)
+    [L, P] pc; e ⊢ TDecl t1 t2 ⇓ Some (Atom v3 Bottom2)
+
+where "[ L , P ] pc ; e ⊢ t ⇓ a" := (eval L P pc e t a).
 
 Hint Constructors eval.
