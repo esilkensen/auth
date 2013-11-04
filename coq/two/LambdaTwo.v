a4c07f27bdbefa812b6a0ddeb2052802bafb71e2
Erik Silkensen <silkense@colorado.edu>
2013-11-04 00:42:10 -0500

    Fixpoint; no induction principle

--- coq/two/LambdaTwo.v
+++ coq/two/LambdaTwo.v
@@ -1,39 +1,45 @@
 Require Export LambdaTwoSyntax.
 Require Export IndistinguishabilityTwo.
 
-
-Fixpoint eval L P pc e t a k {struct k} : Prop :=
-  match t, k with
-    | TNat n, 0 =>
+Fixpoint eval L P pc e t k a {struct k} :=
+  match t with
+    | TNat n =>
       a = Atom (VNat n) pc
-    | TVar n, 0 =>
+    | TVar n =>
       match atIndex e n with
         | Some (Atom v l) =>
           a = Atom v (l ⊔ pc)
         | None => False
       end
-    | TLam t', 0 =>
+    | TLam t' =>
       a = Atom (VClos e t') pc
-    | TApp t1 t2, S k' =>
-      forall e1' t1' l1 a2 a3,
-        eval L P pc e t1 (Atom (VClos e1' t1') l1) k' ->
-        eval L P pc e t2 a2 k' ->
-        eval L P l1 (a2 :: e1') t1' a3 k' ->
-        a = a3
-    | TDecl t1 t2, S k' =>
-      forall e1' t1' l1 a2 a3,
-        eval L P pc e t1 (Atom (VClos e1' t1') l1) k' ->
-        eval L P pc e t2 a2 k' ->
-        eval L P l1 (a2 :: e1') t1' a3 k' ->
-        match a3 with
-          | Atom v3 Bottom2 =>
+    | TApp t1 t2 =>
+      match k with
+        | S k' =>
+          forall e1' t1' l1 a2 a3,
+            eval L P pc e t1 k' (Atom (VClos e1' t1') l1) ->
+            eval L P pc e t2 k' a2 ->
+            eval L P l1 (a2 :: e1') t1' k' a3 ->
             a = a3
-          | Atom v3 Top2 =>
-            (forall a2' e2' v3',
-               env_LPequiv L P (a2 :: e1') (a2' :: e2') ->
-               eval L P l1 (a2' :: e2') t1' (Atom v3' Top2) k' ->
-               value_LPequiv L P v3 v3') ->
-            a = Atom v3 Bottom2
-        end
-    | _, _ => False
+        | 0 => False
+      end
+    | TDecl t1 t2 =>
+      match k with
+        | S k' =>
+          forall e1' t1' l1 a2 v3 l3,
+            eval L P pc e t1 k' (Atom (VClos e1' t1') l1) ->
+            eval L P pc e t2 k' a2 ->
+            eval L P l1 (a2 :: e1') t1' k' (Atom v3 l3) ->
+            match l3 with
+              | Bottom2 =>
+                a = (Atom v3 Bottom2)
+              | Top2 =>
+                (forall a2' e2' v3',
+                   env_LPequiv L P (a2 :: e1') (a2' :: e2') ->
+                   eval L P l1 (a2' :: e2') t1' k' (Atom v3' Top2) ->
+                   value_LPequiv L P v3 v3') ->
+                a = (Atom v3 Bottom2)
+            end
+        | 0 => False
+      end
   end.
--- coq/two/LambdaTwoNI.v
+++ coq/two/LambdaTwoNI.v
@@ -7,47 +7,18 @@ Theorem non_interference_strong_observer :
   forall L (P : value -> value -> Prop) k pc e1 e2 t a1 a2,
     (forall x, P x x) ->
     env_LPequiv L P e1 e2 ->
-    eval L P pc e1 t a1 k ->
-    eval L P pc e2 t a2 k ->
+    eval L P pc e1 t k a1 -> 
+    eval L P pc e2 t k a2 -> 
     atom_LPequiv L P a1 a2.
 Proof.
-  intros L P k.
-  induction k as [| k'];
-    intros pc e1 e2 t a1 a2 Prefl He Heval1 Heval2.
-  - destruct t; unfold eval in *; subst; auto.
-    + (* TNat *)
-      destruct L; destruct pc.
-      * right. left. auto.
-      * left. auto.
-      * right. right. auto.
-      * right. right. auto.
-    + (* TVar *)
-      remember (atIndex e1 n) as o. remember (atIndex e2 n) as o'.
-      destruct o as [a |]; destruct o' as [a' |]; auto.
-      destruct a as [v1 l1]. destruct a' as [v2 l2]. subst.
-      assert (atom_LPequiv L P (Atom v1 l1) (Atom v2 l2)) by
-          (eapply list_forall2_atIndex; eauto).
-      assert (l2 = l1) by
-          (apply atom_LPequiv_lab_inv in H; auto). subst.
-      destruct pc.
-      * rewrite join_bot. assumption.
-      * rewrite join_top. destruct l1; try assumption.
-        apply atom_LPequiv_raise; assumption.
-    + (* TLam *)
-      destruct L; destruct pc.
-      * right. left. auto.
-      * left. auto.
-      * right. right. auto.
-      * right. right. auto.
-  - admit.
-Qed.
+  Admitted.
 
 Theorem non_interference :
   forall L (P : value -> value -> Prop) k pc e1 e2 t a1 a2,
     (forall x, P x x) ->
     env_LPequiv L P e1 e2 ->
-    eval L P pc e1 t a1 k ->
-    eval L P pc e2 t a2 k ->
+    eval L P pc e1 t k a1 ->
+    eval L P pc e2 t k a2 ->
     atom_Lequiv L a1 a2.
 Proof.
   intros L P k pc e1 e2 t a1 a2 Prefl He Heval1 Heval2.
