ff1b59ff578cc28c244b2f12bd3813f06de7cc21
Erik Silkensen <ejs@ccs.neu.edu>
2014-01-07 14:37:18 -0500

    added l index

--- coq/two/Lambda.v
+++ coq/two/Lambda.v
@@ -10,6 +10,24 @@ Proof.
   intros. apply functional_extensionality in H. subst. reflexivity.
 Qed.
 
+Definition pair_lt (a b : nat * nat) : Prop :=
+  fst a + snd a < fst b + snd b.
+
+Lemma pair_lt_wf' :
+  forall s a, fst a + snd a <= s -> Acc pair_lt a.
+Proof.
+  induction s; intros a H1.
+  - destruct a as [a1 a2]; destruct a1; destruct a2;
+    apply Acc_intro; intros y H2; inversion H1; inversion H2.
+  - inversion H1 as [H2 | n H2 Heq]; auto.
+    apply Acc_intro. intros y H3. apply IHs. inversion H3; omega.
+Defined.
+
+Theorem pair_lt_wf : well_founded pair_lt.
+Proof.
+  unfold well_founded; intro; eapply pair_lt_wf'; eauto.
+Defined.
+
 Inductive term : Type :=
   | TBool : bool -> term
   | TNat : nat -> term
@@ -34,10 +52,10 @@ Fixpoint lookup (e : env) (n : nat) : option value :=
       end
   end.
 
-Definition eval_k : nat -> env -> term -> value -> Prop.
+Definition eval_kl : nat * nat -> env -> term -> value -> Prop.
   refine
-    (Fix lt_wf (fun _ => env -> term -> value -> Prop)
-         (fun k eval_k =>
+    (Fix pair_lt_wf (fun _ => env -> term -> value -> Prop)
+         (fun kl eval_kl =>
             fun e t v =>
               match t with
                 | TBool b =>
@@ -49,113 +67,114 @@ Definition eval_k : nat -> env -> term -> value -> Prop.
                 | TAbs t' =>
                   v = VClos e t'
                 | TApp t1 t2 =>
-                  if Compare_dec.zerop k then False
-                  else let eval := eval_k (k - 1) _ in
+                  if Compare_dec.zerop (fst kl) then False
+                  else let eval := eval_kl (fst kl - 1, snd kl) _ in
                        exists e1' t1' v2,
                          eval e t1 (VClos e1' t1') /\
                          eval e t2 v2 /\
                          eval (v2 :: e1') t1' v
               end)).
-  omega.
+  unfold pair_lt. simpl. omega.
 Defined.
 
-Lemma eval_k_eq :
-  forall k,
-    eval_k k =
+Lemma eval_kl_eq :
+  forall kl,
+    eval_kl kl =
     fun e t v =>
       match t with
         | TBool b => v = VBool b
         | TNat n => v = VNat n
         | TVar n => lookup e n = Some v
         | TAbs t' => v = VClos e t'
-        | TApp t1 t2 => (if Compare_dec.zerop k then False
-                         else let eval := eval_k (k - 1) in
+        | TApp t1 t2 => (if Compare_dec.zerop (fst kl) then False
+                         else let eval := eval_kl (fst kl - 1, snd kl) in
                               exists e1' t1' v2,
                                 eval e t1 (VClos e1' t1') /\
                                 eval e t2 v2 /\
                                 eval (v2 :: e1') t1' v)
       end.
 Proof.
-  intro k.
-  apply (Fix_eq lt_wf (fun _ => env -> term -> value -> Prop)); intros.
+  intro kl.
+  apply (Fix_eq pair_lt_wf (fun _ => env -> term -> value -> Prop)); intros;
+  destruct x as [x1 x2].
   apply functional_extensionality; intro e.
   apply functional_extensionality; intro t.
   apply functional_extensionality; intro v.
   destruct t; try reflexivity.
-  destruct x; auto; simpl.
+  destruct x1; auto; simpl.
   apply functional_extensionality_ex; intro e1'.
   apply functional_extensionality_ex; intro t1'.
   apply functional_extensionality_ex; intro v2.
-  assert (H': forall (y : nat), f y = g y) by
+  assert (H': forall (y : nat * nat), f y = g y) by
       (intro; apply functional_extensionality; apply H);
     rewrite H'.
   reflexivity.
 Qed.
 
 Lemma eval_k_bool :
-  forall k e b,
-    eval_k k e (TBool b) (VBool b).
-Proof. intros. rewrite eval_k_eq. reflexivity. Qed.
+  forall k l e b,
+    eval_kl (k, l) e (TBool b) (VBool b).
+Proof. intros. rewrite eval_kl_eq. reflexivity. Qed.
 
-Lemma eval_k_bool_inv :
-  forall k e b v,
-    eval_k k e (TBool b) v -> v = VBool b.
-Proof. intros. destruct v; destruct k; inversion H; reflexivity. Qed.
+Lemma eval_kl_bool_inv :
+  forall k l e b v,
+    eval_kl (k, l) e (TBool b) v -> v = VBool b.
+Proof. intros. destruct v; destruct k; destruct l; inversion H; reflexivity. Qed.
 
-Lemma eval_k_nat :
-  forall k e n,
-    eval_k k e (TNat n) (VNat n).
-Proof. intros. rewrite eval_k_eq. reflexivity. Qed.
+Lemma eval_kl_nat :
+  forall k l e n,
+    eval_kl (k, l) e (TNat n) (VNat n).
+Proof. intros. rewrite eval_kl_eq. reflexivity. Qed.
 
-Lemma eval_k_nat_inv :
-  forall k e n v,
-    eval_k k e (TNat n) v -> v = VNat n.
-Proof. intros. destruct v; destruct k; inversion H; reflexivity. Qed.
+Lemma eval_kl_nat_inv :
+  forall k l e n v,
+    eval_kl (k, l) e (TNat n) v -> v = VNat n.
+Proof. intros. destruct v; destruct k; destruct l; inversion H; reflexivity. Qed.
 
-Lemma eval_k_var :
-  forall k e n v,
-    eval_k k e (TVar n) v -> lookup e n = Some v.
-Proof. intros. rewrite eval_k_eq in H. assumption. Qed.
+Lemma eval_kl_var :
+  forall k l e n v,
+    eval_kl (k, l) e (TVar n) v -> lookup e n = Some v.
+Proof. intros. rewrite eval_kl_eq in H. assumption. Qed.
 
-Lemma eval_k_var_inv :
-  forall k e n v,
-    eval_k k e (TVar n) v -> lookup e n = Some v.
-Proof. intros. destruct v; destruct k; inversion H; reflexivity. Qed.
+Lemma eval_kl_var_inv :
+  forall k l e n v,
+    eval_kl (k, l) e (TVar n) v -> lookup e n = Some v.
+Proof. intros. destruct v; destruct k; destruct l; inversion H; reflexivity. Qed.
 
-Lemma eval_k_abs :
-  forall k e t',
-    eval_k k e (TAbs t') (VClos e t').
-Proof. intros. rewrite eval_k_eq. reflexivity. Qed.
+Lemma eval_kl_abs :
+  forall k l e t',
+    eval_kl (k, l) e (TAbs t') (VClos e t').
+Proof. intros. rewrite eval_kl_eq. reflexivity. Qed.
 
-Lemma eval_k_abs_inv :
-  forall k e t v,
-    eval_k k e (TAbs t) v -> v = VClos e t.
-Proof. intros. destruct v; destruct k; inversion H; reflexivity. Qed.
+Lemma eval_kl_abs_inv :
+  forall k l e t v,
+    eval_kl (k, l) e (TAbs t) v -> v = VClos e t.
+Proof. intros. destruct v; destruct k; destruct l; inversion H; reflexivity. Qed.
 
-Lemma eval_k_app :
-  forall k e t1 t2 e1' t1' v2 v3,
-    eval_k k e t1 (VClos e1' t1') ->
-    eval_k k e t2 v2 ->
-    eval_k k (v2 :: e1') t1' v3 ->
-    eval_k (S k) e (TApp t1 t2) v3.
+Lemma eval_kl_app :
+  forall k l e t1 t2 e1' t1' v2 v3,
+    eval_kl (k, l) e t1 (VClos e1' t1') ->
+    eval_kl (k, l) e t2 v2 ->
+    eval_kl (k, l) (v2 :: e1') t1' v3 ->
+    eval_kl (S k, l) e (TApp t1 t2) v3.
 Proof.
-  intros. rewrite eval_k_eq. simpl.
+  intros. rewrite eval_kl_eq. simpl.
   replace (k - 0) with k by omega.
   exists e1'. exists t1'. exists v2.
   split; try split; assumption.
 Qed.
 
-Lemma eval_k_app_inv :
-  forall k e t1 t2 v,
-    eval_k k e (TApp t1 t2) v ->
+Lemma eval_kl_app_inv :
+  forall k l e t1 t2 v,
+    eval_kl (k, l) e (TApp t1 t2) v ->
     exists k' e1' t1' v2,
       k = S k' /\
-      eval_k k' e t1 (VClos e1' t1') /\
-      eval_k k' e t2 v2 /\
-      eval_k k' (v2 :: e1') t1' v.
+      eval_kl (k', l) e t1 (VClos e1' t1') /\
+      eval_kl (k', l) e t2 v2 /\
+      eval_kl (k', l) (v2 :: e1') t1' v.
 Proof.
   intros.
-  rewrite eval_k_eq in H.
+  rewrite eval_kl_eq in H.
   destruct k; simpl in H.
   - inversion H.
   - destruct H as [e1' [t1' [v2 [H1 [H2 H3]]]]].
