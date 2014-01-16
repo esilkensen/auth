d52847db38d278868c49fe51c9af45585bda8b19
Erik Silkensen <ejs@ccs.neu.edu>
2014-01-16 16:13:21 -0500

    adding LambdaNI.v (missing proofs for Var, App, and Relabel cases)

--- coq/full/LambdaNI.v
+++ coq/full/LambdaNI.v
@@ -1,189 +1,265 @@
-Require Import MyTactics.
-Require Import LabelAlgebra.
+Require Import Classical.
+Require Import Program.Tactics.
 Require Import Lambda.
-Require Import TermEquiv.
-Require Import Auth.
-Require Import BooleanIndistinguishability.
-Require Import Indistinguishability.
-Require Import IndistinguishabilityComparison.
-
-Set Implicit Arguments.
+Require Import LibTactics.
 
 Section NI.
 
-Context {A L: Type} {LA: LabelAlgebra A L} (auth: A) (lab: L).
+Context
+  (L : Type)
+  (M : LabelAlgebra unit L)
+  (l : L)
+  (P : value L -> value L -> Prop)
+  (Prefl: forall v, P v v).
 
-(** * Preliminary lemmas. *)
-(** ** The pc label is below the label of the resulting atom. *)
-Lemma eval_pc_lower_bound :
-  forall pc e t v l,
-    env_auth e ≤ auth ->
-    term_auth t ≤ auth ->
-    pc; e ⊢ t ⇓ Atom v l ->
-    pc ⊑[auth] l.
+Lemma eval_km_mon_n :
+  forall n,
+    forall k m,
+      k + m < n ->
+      (forall pc e t a,
+         eval_km (k, m) l P pc e t a ->
+         eval_km (S k, m) l P pc e t a) /\
+      (forall pc e t a,
+         eval_km (k, S m) l P pc e t a ->
+         eval_km (k, m) l P pc e t a).
 Proof.
-intros pc e t v l Hauthe Hautht Heval.
-remember (Atom v l) as a.
-generalize dependent l. generalize dependent v.
-induction Heval; intros v' l' Heq; inversion Heq; subst; simpl in *; auto.
-* transitivity l1. eauto.
-  apply @eval_auth_upper_bound with (auth := auth) in Heval1; auto.
-  unfold atom_auth in Heval1. fold atom_auth in Heval1. fold (env_auth e1') in Heval1.
-  apply IHHeval3 with (v := v'); auto.
-  + apply @eval_auth_upper_bound with (auth := auth) in Heval2; eauto.
-  + eauto.
-  + eauto.
-* transitivity l1; eauto.
+  intro n. induction n; auto. split; introv Heval.
+  - destruct t.
+    + (* TBool *) 
+      apply eval_km_bool_inv in Heval.
+      subst. apply eval_km_bool.
+    + (* TNat *)
+      apply eval_km_nat_inv in Heval.
+      subst. apply eval_km_nat.
+    + (* TVar *)
+      apply eval_km_var_inv in Heval.
+      destruct Heval as [v' [l' [H1 H2]]].
+      subst. apply eval_km_var. assumption.
+    + (* TAbs *)
+      apply eval_km_abs_inv in Heval.
+      subst. apply eval_km_abs.
+    + (* TApp *)
+      apply eval_km_app_inv in Heval.
+      destruct Heval
+        as [k' [e1' [t1' [l1 [a2 [H1 [H2 [H3 H4]]]]]]]].
+      assert (H2': eval_km (S k', m) l P pc e t1 (Atom (VClos e1' t1') l1))
+        by (apply IHn in H2; try assumption; omega).
+      assert (H3': eval_km (S k', m) l P pc e t2 a2)
+        by (apply IHn in H3; try assumption; omega).
+      assert (H4': eval_km (S k', m) l P l1 (a2 :: e1') t1' a)
+        by (apply IHn in H4; try assumption; omega).
+      clear H2; clear H3; clear H4; subst.
+      apply (eval_km_app (S k') m l P pc e t1 t2 e1' t1' l1 a2 a);
+        assumption.
+    + (* TRelabel *)
+      apply eval_km_relabel_inv in Heval; destruct Heval as [Heval | Heval].
+      * (* E_Relabel1 *)
+        destruct Heval as [k' [v [l1 [H1 [H2 [H3 H4]]]]]].
+        assert (H2': eval_km (S k', m) l P pc e t (Atom v l1))
+          by (apply IHn in H2; try assumption; omega).
+        clear H2; subst.
+        apply (eval_km_relabel_up (S k') m l P pc e t l0 v l1);
+          assumption.
+      * (* E_Relabel2 *)
+        destruct Heval as [k' [v [l1 [H1 [H2 [H3 [H4 H5]]]]]]].
+        assert (H2': eval_km (S k', m) l P pc e t (Atom v l1))
+          by (apply IHn in H2; try assumption; omega).
+        assert (H4': forall e' v',
+                       env_LPequiv L M l P e e' ->
+                       eval_km (m, S k') l P pc e' t (Atom v' l1) ->
+                       value_LPequiv L M l P v v')
+          by (introv He H6; subst;
+              assert (eval_km (m, k') l P pc e' t (Atom v' l1))
+                by (apply IHn in H6; try omega; assumption);
+              apply (H4 e' v'); assumption).
+        clear H2; clear H4; subst.
+        apply (eval_km_relabel_down (S k') m l P pc e t l0 v l1);
+          assumption.
+  - destruct t.
+    + (* TBool *)
+      apply eval_km_bool_inv in Heval.
+      subst. apply eval_km_bool.
+    + (* TNat *)
+      apply eval_km_nat_inv in Heval.
+      subst. apply eval_km_nat.
+    + (* TVar *)
+      apply eval_km_var_inv in Heval.
+      destruct Heval as [v' [l' [H1 H2]]].
+      subst. apply eval_km_var. assumption.
+    + (* TAbs *)
+      apply eval_km_abs_inv in Heval.
+      subst. apply eval_km_abs.
+    + (* TApp *) 
+      apply eval_km_app_inv in Heval.
+      destruct Heval as [k' [e1' [t1' [l1 [a2 [H1 [H2 [H3 H4]]]]]]]].
+      assert (H2': eval_km (k', m) l P pc e t1 (Atom (VClos e1' t1') l1))
+        by (apply IHn; try omega; assumption).
+      assert (H3': eval_km (k', m) l P pc e t2 a2)
+        by (apply IHn; try omega; assumption).
+      assert (H4': eval_km (k', m) l P l1 (a2 :: e1') t1' a)
+        by (apply IHn; try omega; assumption).
+      clear H2; clear H3; clear H4; subst.
+      apply (eval_km_app k' m l P pc e t1 t2 e1' t1' l1 a2 a);
+        assumption.
+    + (* TRelabel *)
+      apply eval_km_relabel_inv in Heval; destruct Heval as [Heval | Heval].
+      * (* E_Relabel1 *)
+        destruct Heval as [k' [v [l1 [H1 [H2 [H3 H4]]]]]].
+        assert (H2': eval_km (k', m) l P pc e t (Atom v l1))
+          by (apply IHn; try omega; assumption).
+        clear H2; subst.
+        apply (eval_km_relabel_up k' m l P pc e t l0 v l1);
+          assumption.
+      * (* E_Relabel2 *)
+        destruct Heval as [k' [v [l1 [H1 [H2 [H3 [H4 H5]]]]]]].
+        assert (H2': eval_km (k', m) l P pc e t (Atom v l1))
+          by (apply IHn; try omega; assumption).
+        assert (H4': forall e' v',
+                       env_LPequiv L M l P e e' ->
+                       eval_km (m, k') l P pc e' t (Atom v' l1) ->
+                       value_LPequiv L M l P v v')
+          by (introv He H6; subst;
+              assert (eval_km (S m, k') l P pc e' t (Atom v' l1))
+                by (apply IHn in H6; try assumption; omega);
+              apply (H4 e' v'); assumption).
+        clear H2; clear H4; subst.
+        apply (eval_km_relabel_down k' m l P pc e t l0 v l1);
+          assumption.
 Qed.
 
-(** ** Strong version of the non-interference theorem. *)
-Lemma NI_strong :
-  forall pc1 pc2 e1 e2 t1 t2 a1 a2,
-    env_auth e1 ≤ auth ->
-    term_auth t1 ≤ auth ->
-    env_auth e2 ≤ auth ->
-    term_auth t2 ≤ auth ->
-    pc1 =L pc2 ->
-    env_Lequiv auth lab e1 e2 ->
-    term_equiv t1 t2 ->
-    pc1; e1 ⊢ t1 ⇓ a1 ->
-    pc2; e2 ⊢ t2 ⇓ a2 ->
-    atom_Lequiv auth lab a1 a2.
+Lemma eval_km_mon :
+  forall k m,
+    (forall pc e t a,
+       eval_km (k, m) l P pc e t a ->
+       eval_km (S k, m) l P pc e t a) /\
+    (forall pc e t a,
+       eval_km (k, S m) l P pc e t a ->
+       eval_km (k, m) l P pc e t a).
 Proof.
-intros pc pc' e e' t t' a a' Hauthe Hautht Hauthe' Hautht' Hpc He Ht Heval.
-generalize dependent a'.
-generalize dependent t'.
-generalize dependent e'.
-generalize dependent pc'.
-induction Heval; intros pc' Hpc e' Hauthe' He t' Hautht' Ht a' Heval';
-destruct t'; simpl in Ht; simpl in Hautht; simpl in Hautht';
-try tauto; inversion Heval'; subst.
-* rename b0 into b. intro Hlab. auto.
-* rename n0 into n. intro Hlab. auto.
-* rename n0 into n. rename v0 into v'. rename l0 into l'.
-  intro Hlab. fold (value_Lequiv auth lab v v').
-  assert (l ⊑[auth] lab \/ l' ⊑[auth] lab) as Hlab'.
-  { destruct Hlab; [left | right].
-    transitivity (l ⊔ pc); auto.
-    transitivity (l' ⊔ pc'); auto.
-  }
-  pose proof (list_forall2_atIndex _ _ _ _ _ _ He H H3) as Hatom.
-  specialize (Hatom Hlab'). fold (value_Lequiv auth lab v v') in Hatom.
-  destruct Hatom as [Hv Hl]. split; auto.
-  rewrite Hl. rewrite Hpc. reflexivity.
-* intro Hlab. fold (atom_Lequiv auth lab). fold (env_Lequiv auth lab e e').
-  splits; auto.
-* rename l0 into l1'. rename a0 into a2'.
-  destruct Ht as [Ht1 Ht2].
-  assert (term_auth t1 ≤ auth) as Hautht1 by eauto.
-  assert (term_auth t2 ≤ auth) as Hautht2 by eauto.
-  clear Hautht.
-  assert (term_auth t'1 ≤ auth) as Hautht'1 by eauto.
-  assert (term_auth t'2 ≤ auth) as Hautht'2 by eauto.
-  clear Hautht'.
-  specialize (IHHeval1 Hauthe Hautht1 _ Hpc _ Hauthe' He _ Hautht'1 Ht1 _ H1).
-  specialize (IHHeval2 Hauthe Hautht2 _ Hpc _ Hauthe' He _ Hautht'2 Ht2 _ H4).
-  destruct a3 as [v3  l3].
-  destruct a' as [v'3 l'3].
-  intro Hlab. fold (value_Lequiv auth lab v3 v'3).
-  assert (l1 ⊑[auth] lab \/ l1' ⊑[auth] lab) as Hlab1.
-  { destruct Hlab; [left | right].
-    + transitivity l3; auto.
-      apply @eval_auth_upper_bound with (auth := auth) in Heval1; auto.
-      simpl in Heval1.
-      apply @eval_auth_upper_bound with (auth := auth) in Heval2; auto.
-      apply eval_pc_lower_bound in Heval3; simpl; eauto.
-    + transitivity l'3; auto.
-      apply @eval_auth_upper_bound with (auth := auth) in H1; auto.
-      simpl in H1.
-      apply @eval_auth_upper_bound with (auth := auth) in H4; auto.
-      apply eval_pc_lower_bound in H6; simpl; eauto.
-  }
-  specialize (IHHeval1 Hlab1).
-  destruct IHHeval1 as [[He1' Ht1'] Hl1].
-  fold (atom_Lequiv auth lab) in He1'.
-  fold (env_Lequiv auth lab e1' e1'0) in He1'.
-  assert (env_auth e1' ∨ atom_auth a2 ≤ auth) as Hautha2e1'.
-  { apply join_lub.
-    + apply @eval_auth_upper_bound with (auth := auth) in Heval1; auto.
-      simpl in Heval1. eauto.
-    + apply @eval_auth_upper_bound with (auth := auth) in Heval2; auto.
-  }
-  assert (term_auth t1' ≤ auth) as Hautht1'.
-  { apply @eval_auth_upper_bound with (auth := auth) in Heval1; auto.
-    simpl in Heval1. eauto.
-  }
-  assert (env_auth e1'0 ∨ atom_auth a2' ≤ auth) as Hautha2'e1'0.
-  { apply join_lub.
-    + apply @eval_auth_upper_bound with (auth := auth) in H1; auto.
-      simpl in H1. eauto.
-    + apply @eval_auth_upper_bound with (auth := auth) in H4; auto.
-  }
-  assert (term_auth t1'0 ≤ auth) as Hautht1'0.
-  { apply @eval_auth_upper_bound with (auth := auth) in H1; auto.
-    simpl in H1. eauto.
-  }
-  specialize
-  (IHHeval3 Hautha2e1' Hautht1'
-            _ Hl1
-            (a2' :: e1'0) Hautha2'e1'0 (conj IHHeval2 He1')
-            _ Hautht1'0 Ht1' _ H6).
-  specialize (IHHeval3 Hlab).
-  fold (value_Lequiv auth lab v3 v'3) in IHHeval3.
+  introv. apply (eval_km_mon_n (S (k + m))). auto.
+Qed.
+
+Lemma eval_km_mon_k_plus_n :
+  forall n k m pc e t a,
+    eval_km (k, m) l P pc e t a ->
+    eval_km (n + k, m) l P pc e t a.
+Proof.
+  intro n. induction n; introv Heval; auto.
+  apply IHn in Heval. apply eval_km_mon in Heval. assumption.
+Qed.
+
+Lemma eval_km_mon_k :
+  forall k k' m pc e t a,
+    k <= k' ->
+    eval_km (k, m) l P pc e t a ->
+    eval_km (k', m) l P pc e t a.
+Proof.
+  introv H Heval.
+  apply (eval_km_mon_k_plus_n (k' - k)) in Heval.
+  assert (H': k' - k + k = k') by omega. rewrite H' in Heval.
   assumption.
-* rename a0 into a'. rename l0 into l'. rename v0 into v1'. rename l3 into l1'.
-  destruct Ht as [Ht [Ha Hl]].
-  rename Hautht into Hauthta.
-  assert (term_auth t ≤ auth) as Hautht by eauto.
-  assert (a ≤ auth) as Hautha by eauto.
-  clear Hauthta.
-  rename Hautht' into Hautht'a'.
-  assert (term_auth t' ≤ auth) as Hautht' by eauto.
-  assert (a' ≤ auth) as Hautha' by eauto.
-  clear Hautht'a'.
-  specialize (IHHeval Hauthe Hautht _ Hpc _ Hauthe' He _ Hautht' Ht _ H6).
-  intro Hlab. fold (value_Lequiv auth lab v1 v1').
-  assert (l1 ⊑[auth]lab \/ l1' ⊑[auth]lab) as Hlab'.
-  { destruct Hlab; [left | right].
-    + transitivity l;  eauto.
-    + transitivity l'; eauto.
-  }
-  specialize (IHHeval Hlab'). fold (value_Lequiv auth lab v1 v1') in IHHeval.
-  destruct IHHeval. split; auto.
 Qed.
 
-(** * General non-interference theorem. *)
-Theorem general_non_interference :
-  forall pc e1 e2 t a1 a2,
-    env_auth e1 ≤ auth ->
-    env_auth e2 ≤ auth ->
-    term_auth t ≤ auth ->
-    env_Lequiv auth lab e1 e2 ->
-    pc; e1 ⊢ t ⇓ a1 ->
-    pc; e2 ⊢ t ⇓ a2 ->
-    atom_Lequiv auth lab a1 a2.
+Lemma eval_km_mon_m_minus_n :
+  forall n k m pc e t a,
+    eval_km (k, n + m) l P pc e t a ->
+    eval_km (k, m) l P pc e t a.
 Proof.
-intros pc e1 e2 t a1 a2 Hauthe1 Hauthe2 Hautht Henv Heval1 Heval2.
-apply NI_strong
-with (pc1 := pc) (pc2 := pc) (e1 := e1) (e2 := e2) (t1 := t) (t2 := t); auto.
+  intro n. induction n; introv Heval; auto.
+  replace (S n + m) with (S (n + m)) in Heval.
+  assert (H: eval_km (k, n + m) l P pc e t a) 
+    by (apply eval_km_mon in Heval; assumption).
+  apply IHn in H. assumption.
+  reflexivity.
 Qed.
 
-(** * Boolean non-interference theorem. *)
-Theorem boolean_non_interference :
-  forall pc e1 e2 t b1 b2 l1 l2,
-    term_auth t ≤ auth ->
-    bool_env_Lequiv auth lab e1 e2 ->
-    pc; e1 ⊢ t ⇓ Atom (VBool b1) l1 ->
-    pc; e2 ⊢ t ⇓ Atom (VBool b2) l2 ->
-    atom_Lequiv auth lab (Atom (VBool b1) l1) (Atom (VBool b2) l2).
+Lemma eval_km_mon_m :
+  forall k m m' pc e t a,
+    m >= m' ->
+    eval_km (k, m) l P pc e t a ->
+    eval_km (k, m') l P pc e t a.
 Proof.
-intros pc e1 e2 t b1 b2 l1 l2 Hautht Henv Heval1 Heval2.
-apply general_non_interference
-with (pc := pc) (e1 := e1) (e2 := e2) (t := t); auto.
-* apply env_bool_Lequiv_auth_bottom_left in Henv. rewrite Henv. auto.
-* apply env_bool_Lequiv_auth_bottom_right in Henv. rewrite Henv. auto.
-* auto using bool_env_Lequiv_env_Lequiv.
+  introv H Heval.
+  remember (m - m') as n.
+  assert (H': m = n + m') by omega.
+  rewrite H' in Heval.
+  apply (eval_km_mon_m_minus_n n k m') in Heval.
+  assumption.
 Qed.
 
+Lemma non_interference_n :
+  forall n k k' m pc e e' t a a',
+    k + k' < n ->
+    k < m ->
+    k' < m ->
+    env_LPequiv L M l P e e' ->
+    eval_km (k, m) l P pc e t a ->
+    eval_km (k', m) l P pc e' t a' ->
+    atom_LPequiv L M l P a a'.
+Proof.
+  intro n. induction n; introv H Hk Hk' He Heval1 Heval2; auto.
+  destruct t.
+  - (* TBool *)
+    apply eval_km_bool_inv in Heval1.
+    apply eval_km_bool_inv in Heval2. subst.
+    apply atom_LPequiv_refl. assumption.
+  - (* TNat *)
+    apply eval_km_nat_inv in Heval1.
+    apply eval_km_nat_inv in Heval2. subst.
+    apply atom_LPequiv_refl. assumption.
+  - (* TVar *)
+    apply eval_km_var_inv in Heval1.
+    apply eval_km_var_inv in Heval2.
+    destruct Heval1 as [v1' [l1' [He1 Ha1]]].
+    destruct Heval2 as [v2' [l2' [He2 Ha2]]].
+    assert (Ha: atom_LPequiv L M l P (Atom v1' l1') (Atom v2' l2'))
+      by (eapply list_forall2_atIndex; eauto). subst.
+    admit.
+  - (* TAbs *)
+    apply eval_km_abs_inv in Heval1.
+    apply eval_km_abs_inv in Heval2. subst.
+    assert (H1: pc ⊑ l \/ ~ pc ⊑ l) by apply classic.
+    destruct H1 as [H1 | H1].
+    + left. auto.
+    + assert (H2: l ⊑ pc \/ ~ l ⊑ pc) by apply classic.
+      destruct H2 as [H2 | H2].
+      * right. left. splits; auto. 
+      * right. right. intro C. destruct C as [C | [C | [C | C]]]; auto.
+  - (* TApp *)
+    apply eval_km_app_inv in Heval1.
+    apply eval_km_app_inv in Heval2.
+    destruct Heval1
+      as [k1' [e11' [t11' [l11 [a21 [H1 [H2 [H3 H4]]]]]]]].
+    destruct Heval2
+      as [k1'' [e11'' [t11'' [l11' [a21' [H1' [H2' [H3' H4']]]]]]]].
+    remember (Atom (VClos e11' t11') l11) as a1.
+    remember (Atom (VClos e11'' t11'') l11') as a1'.
+    assert (Ha1: atom_LPequiv L M l P a1 a1')
+      by (apply (IHn k1' k1'' m pc e e' t1 a1 a1') in H2;
+          try assumption; omega); subst.
+    admit.
+  - (* TRelabel *)
+    apply eval_km_relabel_inv in Heval1.
+    apply eval_km_relabel_inv in Heval2.
+    admit.
+Qed.
+
+Theorem non_interference :
+  forall pc e e' t a a',
+    env_LPequiv L M l P e e' ->
+    eval l P pc e t a ->
+    eval l P pc e' t a' ->
+    atom_LPequiv L M l P a a'.
+Proof.
+  introv He Heval1 Heval2.
+  unfold eval in *.
+  destruct Heval1 as [k Heval1].
+  destruct Heval2 as [k' Heval2].
+  assert (H1: eval_km (k, (S (k + k'))) l P pc e t a) by apply Heval1.
+  assert (H2: eval_km (k', (S (k + k'))) l P pc e' t a') by apply Heval2.
+  remember (S (k + k')) as n.
+  apply (non_interference_n n k k' n pc e e' t a a');
+    try omega; assumption.
+Qed.
 
 End NI.
